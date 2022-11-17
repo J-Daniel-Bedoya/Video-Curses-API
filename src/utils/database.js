4da8d6bab4393708c9f5de547677732698e271d3
daniel1718 <jbedoyachavarriaga@gmail.com>
2022-11-17 12:14:02 -0500

    new elements

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,7 +3,6 @@ const { Sequelize } = require('sequelize');
 const db = new Sequelize  ({
   database: process.env.DB_NAME, 
   username: process.env.DB_USER, 
-  port: process.env.DB_PORT, 
   host: process.env.DB_HOST, 
   password: process.env.DB_PASSWORD, 
   dialect: "postgres",
