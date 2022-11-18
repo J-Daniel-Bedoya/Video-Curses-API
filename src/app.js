70c4ad495ef9f11e49209f0d5403a5e24e30dd07
daniel1718 <jbedoyachavarriaga@gmail.com>
2022-11-17 23:27:16 -0500

    add elements

--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ const express = require('express');
 const db = require('./utils/database');
 const initModels = require("./models/initModels");
 const handleError = require("./middlewares/error");
-const docsUsers = require("./docs/users.docs.json");
-const docsCourses = require("./docs/courses.docs.json");
+const { docsUsers, docsCourses, docsVideos, docsCategories }  = require("./docs");
 
 const app = express();
 app.use(express.json());
@@ -20,7 +19,7 @@ db.sync({ alter: true })
 initModels();
 
 app.get('/', (req, res) => {
-  res.status(200).json({docsUsers, docsCourses})
+  res.status(200).json({docsUsers, docsCourses, docsVideos, docsCategories})
 });
   
 app.use("/api/v1", require("./routes"))
---
+++ src/docs/index.js
@@ -0,0 +1,11 @@
+const docsUsers = require("./users.docs.json");
+const docsCourses = require("./courses.docs.json");
+const docsVideos = require("./videos.docs.json")
+const docsCategories = require("./categories.docs.json")
+
+module.exports = {
+  docsUsers,
+  docsCourses,
+  docsVideos,
+  docsCategories,
+}
