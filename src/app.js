19118676dbd7e410dd10a30e0e8e6fbb23c885fb
daniel1718 <jbedoyachavarriaga@gmail.com>
2022-11-17 22:52:11 -0500

    add rutas

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const db = require('./utils/database');
 const initModels = require("./models/initModels");
 const handleError = require("./middlewares/error");
+const docsUsers = require("./docsUsers.json");
 
 const app = express();
 app.use(express.json());
@@ -18,42 +19,7 @@ db.sync({ alter: true })
 initModels();
 
 app.get('/', (req, res) => {
-  res.status(200).json({
-    "status": 'Respuesta exitosa',
-    "url_base": "https://video-curses-api-production.up.railway.app/api/v1",
-    "todas_las_rutas": [
-      { "get_para_ver_todos_los_usuarios": "/users" },
-      { "get_para_ver_los_datos_de_un_usuario_en_detalle": "users/:id" },
-      { "get_para_ver_los_cursos_del_usuario": "/users/:id/courses" },
-      {
-        "post_para_crear_un_nuevo_usuario": {
-          "ruta": "/users",
-          "ejemplo": {
-            "name": "Introdusca su nombre o un apodo",
-            "email": "Colaca el email que prefiera",
-            "password": "Introdusca la contraseña que quiera"
-          }
-        }
-      },
-      {
-        "put_para_actualizar_la_información_del_ususario_solo_name_y_password": {
-          "ruta": "/users/:id",
-          "ejemplo": {
-            "name": "Introdusca un nuevo nombre o un apodo",
-            "password": "Introdusca la nueva contraseña que quiera"
-          }
-        }
-      },
-      {
-        "post_para_que_un_usuario_se_agrege_un_curso": {
-          "ruta": "/users/:id",
-          "ejemplo": {
-            "courseId": "Introdusca el id del curso que desea"
-          }
-        }
-      }
-    ]
-  })
+  res.status(200).json(docsUsers)
 });
   
 app.use("/api/v1", require("./routes"))
